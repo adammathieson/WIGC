50ad9b6eecff1964c3ded217c79aba22b84dcc40
Adam Mathieson <mathieson1222@gmail.com>
2020-02-12 21:42:37 -0800

    mobile menu closes when click off the menu or burger

--- js/app.js
+++ js/app.js
@@ -1,10 +1,37 @@
+const menu = document.querySelector('.nav-list')
+const burger = document.querySelector('.burger')
+// const body = document.querySelector("body")
+
+
 const toggleMenu = () => {
-    const menu = document.querySelector('.nav-list')
-    const burger = document.querySelector('.burger')
 
     burger.addEventListener('click', () => {
         menu.classList.toggle('toggle')
     })
+    
+    // Hides menu when resizing to mobile view
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 500) {
+            menu.classList.remove('toggle')
+        } else {
+            menu.classList.add('toggle')
+        }        
+    })
+}
+
+const clickOffClose = () => {
+    window.addEventListener('mouseup', () => {
+        if (
+            !menu.classList.contains('toggle') &&
+            event.target !== menu && 
+            event.target !== burger
+        ) {
+            menu.classList.add('toggle')            
+            }
+        
+    })
+    
 }
 
 toggleMenu()
+clickOffClose()
