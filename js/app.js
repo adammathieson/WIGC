ac2780f6ae8ad7f995a765a64c4fde70f1575fa7
Adam Mathieson <mathieson1222@gmail.com>
2020-02-13 23:28:10 -0800

    adds forEach to navLinks and exludes from mousup event trigger; adds a header tag for styling

--- js/app.js
+++ js/app.js
@@ -2,9 +2,12 @@
 const toggleMenu = () => {
     const menu = document.querySelector('.nav-list')
     const burger = document.querySelector('.burger')
+    const navLinks = document.querySelectorAll('.nav-list li a')
+    console.log(navLinks)
     
     burger.addEventListener('click', () => {
         menu.classList.toggle('toggle')
+        // console.log('click')
     })
     
     // Hides menu when resizing to mobile view
@@ -21,7 +24,8 @@ const toggleMenu = () => {
         if (
             !menu.classList.contains('toggle') &&
             event.target !== menu && 
-            event.target !== burger
+            event.target !== burger && 
+            navLinks.forEach(link => link !== event.target)
         ) {
             menu.classList.add('toggle')            
             }
