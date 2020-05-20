8c41da9934c5a7e296a1af503236f9300ac47d7b
Adam Mathieson <mathieson1222@gmail.com>
2020-05-19 17:37:58 -0700

    js added for 2nd part of the luna animtion

--- js/home/lunaAnimation.js
+++ js/home/lunaAnimation.js
@@ -2,8 +2,14 @@
 export const lunaAnimation = () => {
     const hatch = document.querySelector('.car-hatch')
     const luna = document.querySelector('.luna')
+    const lunaFaceView = document.querySelector('.luna-fv')
 
     hatch.addEventListener('animationstart', () => {
         luna.classList.remove('hide')
+    })
+
+    luna.addEventListener('animationend', () => {
+        luna.classList.add('hide')
+        lunaFaceView.classList.remove('hide')
     })
 }
