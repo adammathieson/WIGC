4c03ba5d289d4277685642581a23cab479511a49
Adam Mathieson <mathieson1222@gmail.com>
2020-06-02 23:43:39 -0700

    refactors gooseAnimation with a simplified classlist update for each rendered goose

--- js/home/gooseAnimation.js
+++ js/home/gooseAnimation.js
@@ -12,15 +12,16 @@ export const gooseAnimation = () => {
         geese.forEach(goose => {
             let zone = goose.parentElement.classList.value
             // console.log(goose)
-            if (zone === 'goose-zone1') {
-                goose.classList.add('pre-fly-zone1')
-            }
-            if (zone === 'goose-zone2') {
-                goose.classList.add('pre-fly-zone2')
-            }
-            if (zone === 'goose-zone3') {
-                goose.classList.add('pre-fly-zone3')
-            }
+            goose.classList.add(`pre-fly-${zone}`)
+            // if (zone === 'goose-zone1') {
+            //     goose.classList.add('pre-fly-zone1')
+            // }
+            // if (zone === 'goose-zone2') {
+            //     goose.classList.add('pre-fly-zone2')
+            // }
+            // if (zone === 'goose-zone3') {
+            //     goose.classList.add('pre-fly-zone3')
+            // }
         })
     })
 
